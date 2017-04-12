e981c2378fcda09a442764eea88ad4b11dbac9b3
Andrebitbucket <GamAndre777@mail.ru>
2017-04-12 22:16:46 +0500

    Kubik

---
+++ 1.bat
@@ -0,0 +1,5 @@
+rem del 1.os
+rem cat UfaScript.bl test.os >1.os
+rem oscript ListFile.os >1.os
+oscript test.os
+
---
+++ Kubik.bat
@@ -0,0 +1,3 @@
+cat UfaScript.osb Kubik.os >2.os
+rem oscript ListFile.os >1.os
+oscript 2.os
